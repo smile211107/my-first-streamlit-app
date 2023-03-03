430598380450861794f33f1b52848b17637d3e79
smile211107 <117446738+smile211107@users.noreply.github.com>
2023-03-03 22:02:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ st.title("GIẢI PHƯƠNG TRÌNH BẬC NHẤT")
 a = st.number_input('tham số a')
 b = st.number_input('tham số b')
 if (a==0):
-  res = "Phương trình vô nghiệm"
+  if (b != 0):
+    res = "Phương trình vô nghiệm"
+  else:
+    res = "Phương trình vô số nghiệm"
 else:
   res = "Phương trình có 1 nghiệm " + str(-b / a)
 if st.button('Giải'):
